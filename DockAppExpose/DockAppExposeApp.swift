7a0658afe9631956676322bef5ce1c48192d132e
Alex <apotenza92@gmail.com>
2025-12-27 15:32:48 +1100

    Initial Commit

---
+++ DockAppExpose/DockAppExposeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockAppExposeApp.swift
+//  DockAppExpose
+//
+//  Created by Alex on 27/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct DockAppExposeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
